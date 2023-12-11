3392a9b81ff4cdb5177fd1e6dd7a0e5b1062c6f6
Travis Walton <2472981+waltont8@users.noreply.github.com>
2023-12-11 19:58:41 +0000

    Day 11

--- src/Day11.hs
+++ src/Day11.hs
@@ -9,13 +9,27 @@ import qualified Data.Set as S
 type Coord = (Int, Int)
 type Grid = M.Map Coord Char
 
+-- Part 1 and part 2 are the same so I just changed the number from 2 to 1000000 and dropped part 2
+-- bit messy but it's a Monday morning and I am busy
 day11 :: String -> (String, String)
-day11 = lines >>> expandY >>> transpose >>> expandY >>> transpose >>> parse >>> (part1 &&& part2) >>> tidy
+day11 input = (show $ part g', "")
+    where
+        l = lines input
+        blankRows = getBlankRows 0 l
+        blankCols = getBlankRows 0 $ transpose l
+        g = parse l
+        g' = M.fromList $ map (stretch 1000000 blankRows blankCols) $ filter (\(_,c) -> c=='#') $ M.toList g
 
-expandY (h:xs) 
-    | all (=='.') h = h:h:expandY xs
-    | otherwise = h:expandY xs
-expandY [] = []
+stretch :: Int -> [Int] -> [Int] -> (Coord, Char) -> (Coord, Char)
+stretch fact rows cols ((x,y),c) = ((x+xm, y+ym),c)
+    where
+        xm = (*(fact-1)) $ length $ filter (<x) cols
+        ym = (*(fact-1)) $ length $ filter (<y) rows
+
+getBlankRows n (h:xs)
+    | all (=='.') h = n:getBlankRows (n+1) xs
+    | otherwise = getBlankRows (n+1) xs
+getBlankRows _ [] = []
 
 parse :: [String] -> Grid
 parse input =  foldl (\m (n,c) -> M.insert (n `mod` width, n `div` width) c m) (M.empty :: Grid) $ zip [0..] (concat input)
@@ -23,17 +37,14 @@ parse input =  foldl (\m (n,c) -> M.insert (n `mod` width, n `div` width) c m) (
         width = length (input!!0)
 
 
-part1 :: Grid -> Int
-part1 g = sum distances
+part :: Grid -> Int
+part g = sum distances
     where
         galaxies = filter (\((x,y),c) -> c == '#') $ M.toList g
         allCombos = [(x,y) | x<-galaxies, y<-galaxies, x<y]
         distances = map distancePair allCombos
 
 distancePair (((x1,y1), c1), ((x2,y2), c2)) = abs (x1-x2) + abs (y1-y2)
-
-part2 :: Grid -> Int -- the whole grid - the path - the outersquares
-part2 g = length $ M.toList g
 
 tidy :: (Int, Int) -> (String, String)
 tidy (a,b) = (show a, show b)
